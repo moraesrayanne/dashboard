dcafd17c9f11af94215d26a1b279e1ba2fd9d4c9
Rayanne Moraes <moraesrayanne@hotmail.com>
2025-04-29 20:24:45 -0300

    fix: btn styles

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -5,17 +5,17 @@ interface ButtonComponentProps {
 }
 
 export const Button = ({ onClick, variant = "solid", children }: ButtonComponentProps) => {
-  const baseClasses =
-    "w-full h-10 rounded-lg text-sm font-medium active:scale-98 focus:outline-none focus:ring-0 transition-colors duration-200 cursor-pointer"
-
   const variants = {
-    solid: `bg-blue-500 text-white hover:bg-blue-400 ${baseClasses}`,
-    outline: `border border-blue-400 text-blue-500 hover:bg-blue-50 ${baseClasses}`,
-    text: `text-blue-500 hover:bg-blue-100 ${baseClasses}`,
+    solid: "bg-blue-500 text-white hover:bg-blue-400 ${baseClasses}",
+    outline: "border border-blue-400 text-blue-500 hover:bg-blue-50 ${baseClasses}",
+    text: "text-blue-500 hover:bg-blue-100",
   }
 
   return (
-    <button onClick={onClick} className={variants[variant]}>
+    <button
+      onClick={onClick}
+      className={`${variants[variant]} w-full h-10 rounded-lg text-sm font-medium active:scale-98 focus:outline-none focus:ring-0 transition-colors duration-200 cursor-pointer`}
+    >
       {children}
     </button>
   )
