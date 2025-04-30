6a54bc5f6a35b77ae5fbd2296b75994b589a321c
Rayanne Moraes <moraesrayanne@hotmail.com>
2025-04-29 22:13:59 -0300

    feat: add type submit to button component

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,10 +1,13 @@
-interface ButtonComponentProps {
-  onClick: () => void
+import { ButtonHTMLAttributes } from "react"
+
+interface ButtonComponentProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  onClick?: () => void
   variant?: "solid" | "outline" | "text"
   children: string
+  type?: "submit" | "button"
 }
 
-export const Button = ({ onClick, variant = "solid", children }: ButtonComponentProps) => {
+export const Button = ({ onClick, variant = "solid", type = "button", children }: ButtonComponentProps) => {
   const variants = {
     solid: "bg-blue-500 text-white hover:bg-blue-400 ${baseClasses}",
     outline: "border border-blue-400 text-blue-500 hover:bg-blue-50 ${baseClasses}",
@@ -14,6 +17,7 @@ export const Button = ({ onClick, variant = "solid", children }: ButtonComponent
   return (
     <button
       onClick={onClick}
+      type={type}
       className={`${variants[variant]} w-full h-10 rounded-lg text-sm font-medium active:scale-98 focus:outline-none focus:ring-0 transition-colors duration-200 cursor-pointer`}
     >
       {children}
