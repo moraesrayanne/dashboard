0faf852b54fd42b762767101bc9b133f01d21c5f
Rayanne Moraes <moraesrayanne@hotmail.com>
2025-04-29 22:14:28 -0300

    feat: create input error state

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -2,18 +2,22 @@ import { InputHTMLAttributes } from "react"
 
 interface InputComponentProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+  error?: string | null
 }
 
-export const Input = ({ label, onChange, ...props }: InputComponentProps) => {
+export const Input = ({ label, onChange, error, ...props }: InputComponentProps) => {
   return (
-    <div className="w-[100%] flex flex-col">
-      {label && <label>{label}</label>}
+    <div className="w-full flex flex-col gap-1">
+      {label && <label className={`text-sm font-medium ${error ? "text-red-500" : "text-gray-700"}`}>{label}</label>}
       <input
         {...props}
         onChange={onChange}
-        className="border rounded-sm border-gray-400 p-2 h-10 text-gray-700 focus:outline-none  focus:border-blue-500 placeholder:text-sm"
+        className={`border rounded-sm p-2 h-10 text-gray-700 focus:outline-none 
+          ${error ? "border-red-500 focus:border-red-500" : "border-gray-400 focus:border-blue-500"} 
+          placeholder:text-sm`}
       />
+      {error && <span className="text-xs text-red-500">{error}</span>}
     </div>
   )
 }
