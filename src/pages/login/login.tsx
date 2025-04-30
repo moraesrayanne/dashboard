5566a15391727dbfdca217b0a61393625ea6e677
Rayanne Moraes <moraesrayanne@hotmail.com>
2025-04-29 22:15:39 -0300

    feat: validating form with zod and react-hook-form

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,27 +1,54 @@
-import { useState } from "react"
 import { Input } from "@components/input/input"
 import { Button } from "@components/button/button"
+import { SubmitHandler, useForm } from "react-hook-form"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+interface IFormInput {
+  email: string
+  password: string
+}
+
+const schema = z.object({
+  email: z.string().email({ message: "Formato de email incorreto" }),
+  password: z.string().min(8, { message: "O email precisa ter no mínimo 8 caracteres" }),
+})
 
 export const Login = () => {
-  const [email, setEmail] = useState<string>()
-  const [password, setPassword] = useState<string>()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>({ resolver: zodResolver(schema) })
 
-  const credentials = {
-    email,
-    password,
-  }
+  const onSubmit: SubmitHandler<IFormInput> = async (data) => console.log(data)
+
+  const createAccount = () => console.log("Criando a conta...")
 
   return (
-    <div className="h-dvh flex justify-center">
-      <div className="p-10 flex-col w-120 border shadow-sm border-gray-200 rounded-md self-center flex gap-5">
-        <span className="text-3xl text-center">Bem vindo</span>
-        <Input placeholder='usuario@email.com' label="E-mail" onChange={(e) => setEmail(e.target.value)} />
-        <Input type='password' placeholder='********' label="Senha" onChange={(e) => setPassword(e.target.value)} />
-        <Button onClick={() => console.log(credentials)}>Entrar</Button>
-        <Button variant="outline" onClick={() => console.log(credentials)}>
-          Criar conta
-        </Button>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="h-dvh flex justify-center">
+        <div className="p-10 flex-col w-120 border shadow-sm border-gray-200 rounded-md self-center flex gap-5">
+          <span className="text-3xl text-center">Bem vindo</span>
+          <Input
+            placeholder="usuario@email.com"
+            label="E-mail"
+            error={errors["email"]?.message}
+            {...register("email")}
+          />
+          <Input
+            type="password"
+            placeholder="************"
+            label="Senha"
+            error={errors["password"]?.message}
+            {...register("password")}
+          />
+          <Button type="submit">Entrar</Button>
+          <Button variant="outline" onClick={createAccount}>
+            Criar conta
+          </Button>
+        </div>
       </div>
-    </div>
+    </form>
   )
 }
